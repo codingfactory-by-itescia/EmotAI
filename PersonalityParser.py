4965abb45479a52993856cf9b32350b50edd623b
Thomas Guillouët <thomas.guillouet@outlook.com>
2021-03-25 14:56:41 +0100

    Add gitignore and personality parser

---
+++ PersonalityParser.py
@@ -0,0 +1,14 @@
+import json
+
+
+class PersonalityParser:
+    @staticmethod
+    def parse(personality_file_name):
+        file = open("./personalities/" + personality_file_name + ".json")
+        return json.loads(file.read())
+
+    @staticmethod
+    def save(personality_file_name, values):
+        file = open("./personalities/" + personality_file_name + ".json", "w")
+        json_values = json.dumps(values)
+        return file.write(json_values)
--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-# words = [{"Dog",3}]
-# print(words[0])
-# name = input("Hello, can you give me my name ? ")
-# number = int(input("Please, give me an integer number "))
-# print("Okay, my number is "+str(number)+" and my name is \""+name+"\" !")
 from AIConversation import AIConversation
 from EmotAI import EmotAI
+from PersonalityParser import PersonalityParser
 
-c = AIConversation()
+loadedPersonality = PersonalityParser.parse("personality1")
+
+loadedPersonality['sensible'] = True
+
+PersonalityParser.save("personality1", loadedPersonality)
+
+# c = AIConversation()
+
